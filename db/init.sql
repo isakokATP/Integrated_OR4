5a80014e397215c88dd679dd1ae9ea4589fbd0b3
Shisanucha Chengsamo <110714943+isakokATP@users.noreply.github.com>
2025-04-30 22:07:10 +0700

    65130500019-[Update]-init.sql

--- db/init.sql
+++ db/init.sql
@@ -6,61 +6,61 @@ USE pbi1;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- Drop tables
-DROP TABLE IF EXISTS SaleItem;
-DROP TABLE IF EXISTS Brand;
+DROP TABLE IF EXISTS sale_items;
+DROP TABLE IF EXISTS brands;
 
 SET FOREIGN_KEY_CHECKS = 1;
 
 -- CREATE TABLE Brand
-CREATE TABLE Brand (
-                       id INT AUTO_INCREMENT PRIMARY KEY,
-                       name VARCHAR(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL CHECK (TRIM(name) <> '') ,
-                       websiteUrl VARCHAR(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL CHECK (TRIM(websiteUrl) IS NULL OR TRIM(websiteUrl) <> '')  ,
-                       isActive BOOLEAN NOT NULL DEFAULT TRUE,
-                       countryOfOrigin VARCHAR(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL CHECK (TRIM(countryOfOrigin) IS NULL OR TRIM(countryOfOrigin) <> ''),
-                       createdOn DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                       updatedOn DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+CREATE TABLE brands (
+                        id INT AUTO_INCREMENT PRIMARY KEY,
+                        name VARCHAR(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL CHECK (TRIM(name) <> '') ,
+                        website_url VARCHAR(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL CHECK (TRIM(website_url) IS NULL OR TRIM(website_url) <> '')  ,
+                        is_active BOOLEAN NOT NULL DEFAULT TRUE,
+                        country_of_origin VARCHAR(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL CHECK (TRIM(country_of_origin) IS NULL OR TRIM(country_of_origin) <> ''),
+                        created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                        updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 -- CREATE TABLE SaleItem
-CREATE TABLE SaleItem (
-                          id INT AUTO_INCREMENT PRIMARY KEY,
-                          model VARCHAR(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL CHECK (TRIM(model) <> ''),
-                          brand_id INT,
-                          description TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL CHECK (TRIM(description) <> ''),
-                          price INT NOT NULL CHECK (price >= 0),
-                          ramGb INT DEFAULT NULL CHECK (ramGb IS NULL OR ramGb >= 0),
-                          screenSizeInch DECIMAL(3,1) DEFAULT NULL,
-                          storageGb INT DEFAULT NULL,
-                          color VARCHAR(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL CHECK (color IS NULL OR TRIM(color) <> ''),
-                          quantity INT NOT NULL DEFAULT 1,
-                          createdOn DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                          updatedOn DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                          FOREIGN KEY (brand_id) REFERENCES Brand(id)
+CREATE TABLE sale_items (
+                            id INT AUTO_INCREMENT PRIMARY KEY,
+                            model VARCHAR(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL CHECK (TRIM(model) <> ''),
+                            brand_id INT,
+                            description TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL CHECK (TRIM(description) <> ''),
+                            price INT NOT NULL CHECK (price >= 0),
+                            ram_gb INT DEFAULT NULL CHECK (ram_gb IS NULL OR ram_gb >= 0),
+                            screen_size_inch DECIMAL(3,1) DEFAULT NULL,
+                            storage_gb INT DEFAULT NULL,
+                            color VARCHAR(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL CHECK (color IS NULL OR TRIM(color) <> ''),
+                            quantity INT NOT NULL DEFAULT 1,
+                            created_on DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                            updated_on DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                            FOREIGN KEY (brand_id) REFERENCES brands(id)
 );
 
 -- INSERT ข้อมูลลงใน Table Brand
-INSERT INTO Brand (name, countryOfOrigin, websiteUrl, isActive) VALUES
-                                                                    ('Samsung', 'South Korea', 'https://www.samsung.com', TRUE),
-                                                                    ('Apple', 'United States', 'https://www.apple.com', TRUE),
-                                                                    ('Xiaomi', 'China', 'https://www.mi.com', TRUE),
-                                                                    ('Huawei', 'China', 'https://www.huawei.com', TRUE),
-                                                                    ('OnePlus', 'China', 'https://www.oneplus.com', TRUE),
-                                                                    ('Sony', 'Japan', 'https://www.sony.com', TRUE),
-                                                                    ('LG', 'South Korea', 'https://www.lg.com', TRUE),
-                                                                    ('Nokia', 'Finland', 'https://www.nokia.com', FALSE),
-                                                                    ('Motorola', 'United States', 'https://www.motorola.com', FALSE),
-                                                                    ('OPPO', 'China', 'https://www.oppo.com', TRUE),
-                                                                    ('Vivo', 'China', 'https://www.vivo.com', TRUE),
-                                                                    ('ASUS', 'Taiwan', 'https://www.asus.com', TRUE),
-                                                                    ('Google', 'United States', 'https://store.google.com', TRUE),
-                                                                    ('Realme', 'China', 'https://www.realme.com', TRUE),
-                                                                    ('BlackBerry', 'Canada', 'https://www.blackberry.com', TRUE),
-                                                                    ('HTC', 'Taiwan', 'https://www.htc.com', TRUE),
-                                                                    ('ZTE', 'China', 'https://www.zte.com', TRUE),
-                                                                    ('Lenovo', 'China', 'https://www.lenovo.com', TRUE),
-                                                                    ('Honor', 'China', 'https://www.hihonor.com', TRUE),
-                                                                    ('Nothing', 'United Kingdom', 'https://nothing.tech', TRUE);
+INSERT INTO brands (name, country_of_origin, website_url, is_active) VALUES
+                                                                         ('Samsung', 'South Korea', 'https://www.samsung.com', TRUE),
+                                                                         ('Apple', 'United States', 'https://www.apple.com', TRUE),
+                                                                         ('Xiaomi', 'China', 'https://www.mi.com', TRUE),
+                                                                         ('Huawei', 'China', 'https://www.huawei.com', TRUE),
+                                                                         ('OnePlus', 'China', 'https://www.oneplus.com', TRUE),
+                                                                         ('Sony', 'Japan', 'https://www.sony.com', TRUE),
+                                                                         ('LG', 'South Korea', 'https://www.lg.com', TRUE),
+                                                                         ('Nokia', 'Finland', 'https://www.nokia.com', FALSE),
+                                                                         ('Motorola', 'United States', 'https://www.motorola.com', FALSE),
+                                                                         ('OPPO', 'China', 'https://www.oppo.com', TRUE),
+                                                                         ('Vivo', 'China', 'https://www.vivo.com', TRUE),
+                                                                         ('ASUS', 'Taiwan', 'https://www.asus.com', TRUE),
+                                                                         ('Google', 'United States', 'https://store.google.com', TRUE),
+                                                                         ('Realme', 'China', 'https://www.realme.com', TRUE),
+                                                                         ('BlackBerry', 'Canada', 'https://www.blackberry.com', TRUE),
+                                                                         ('HTC', 'Taiwan', 'https://www.htc.com', TRUE),
+                                                                         ('ZTE', 'China', 'https://www.zte.com', TRUE),
+                                                                         ('Lenovo', 'China', 'https://www.lenovo.com', TRUE),
+                                                                         ('Honor', 'China', 'https://www.hihonor.com', TRUE),
+                                                                         ('Nothing', 'United Kingdom', 'https://nothing.tech', TRUE);
 
 -- แสดงข้อมูลใน Table Brand
-SELECT * FROM Brand;
+SELECT * FROM brands;
