70188f80d0a6f54f3e68bc3ac72dc123a7d7945a
aivane <110761536+aivane@users.noreply.github.com>
2025-05-05 13:33:53 +0700

    65130500101-[CREATE]-cypress

---
+++ Oct_frontend/cypress/support/e2e.js
@@ -0,0 +1,5 @@
+// This file is processed and loaded automatically before your test files.
+// You can use this space to add global configuration and behavior that modifies Cypress.
+
+// For example, you can import custom commands here:
+// import './commands'
