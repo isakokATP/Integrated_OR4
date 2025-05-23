101bbc174ab319df00b7fdf9b115dcc9941105e7
Shisanucha Chengsamo <110714943+isakokATP@users.noreply.github.com>
2025-05-24 02:11:02 +0700

    65130500016-[update]-pbi10,11,12BE

--- Oct_backend/src/main/java/com/int221/int221backend/repositories/SaleItemRepository.java
+++ Oct_backend/src/main/java/com/int221/int221backend/repositories/SaleItemRepository.java
@@ -1,7 +1,11 @@
 package com.int221.int221backend.repositories;
 
 import com.int221.int221backend.entities.SaleItem;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -18,4 +22,6 @@ public interface SaleItemRepository extends JpaRepository <SaleItem, Integer> {
 
     List<SaleItem> findAllByOrderByModelAsc();
     List<SaleItem> findByQuantityGreaterThanOrderByBrand_NameAsc(Integer quantity);
+    @Query("select st from SaleItem st where st.brand.name IN :brandList ")
+    Page<SaleItem> findAll(Pageable pageable, @Param("brandList") List<String> brandList);
 }
