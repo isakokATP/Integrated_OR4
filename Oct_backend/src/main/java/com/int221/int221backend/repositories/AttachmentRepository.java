f97a4ce738614aecb600b4f8dc7c133cdd9c11b6
Shisanucha Chengsamo <110714943+isakokATP@users.noreply.github.com>
2025-08-17 14:37:55 +0700

    65130500016-[int222]-[fixpbi15]

--- Oct_backend/src/main/java/com/int221/int221backend/repositories/AttachmentRepository.java
+++ Oct_backend/src/main/java/com/int221/int221backend/repositories/AttachmentRepository.java
@@ -1,7 +1,9 @@
 package com.int221.int221backend.repositories;
 
 import com.int221.int221backend.entities.Attachment;
+import com.int221.int221backend.entities.SaleItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AttachmentRepository extends JpaRepository<Attachment, Integer> {
+    Integer countAttachmentBySaleItem(SaleItem saleItem);
 }
