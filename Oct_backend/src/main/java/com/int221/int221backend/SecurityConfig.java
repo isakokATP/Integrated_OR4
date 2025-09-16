b09bf9da581057439356ed931693ca5b6afde188
Shisanucha Chengsamo <110714943+isakokATP@users.noreply.github.com>
2025-09-16 13:07:21 +0700

    65130500016-[int22]-[fixunautherized]

--- Oct_backend/src/main/java/com/int221/int221backend/SecurityConfig.java
+++ Oct_backend/src/main/java/com/int221/int221backend/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @Configuration
@@ -19,15 +20,19 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // ปิด CSRF สำหรับ REST API
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/itb-mshop/v2/users/register",
-                                "/itb-mshop/v2/auth/verify-email"
+                                "/itb-mshop/v2/auth/verify-email",
+                                "/itb-mshop/v2/sale-items/**"
+                        ).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher(
+                                "/itb-mshop/v2/sale-items/*/attachments/by-order", "DELETE")
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults()); // ใช้ Basic Auth สำหรับ request อื่น ๆ
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
